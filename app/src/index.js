03b5d5a4855eb15e99b439c4b9027b414a5c67c1
zoomido <me@zoomido.se>
2019-05-10 10:04:28 +0200

    updated square component

--- app/src/index.js
+++ app/src/index.js
@@ -3,20 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -34,7 +27,7 @@ class Board extends React.Component {
     return
       <Square
         value={this.state.squares[i]}
-        onClick=() 
+        onClick={() => this.handleClick(i)}
       />;
   }
 
